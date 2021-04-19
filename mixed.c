c4f51615f0bf0dfca0c7b7b6a1de7e06eb6953a1
Aditya Bhimesha <adityabhimesha@Adityas-MacBook-Pro.local>
2021-04-19 11:39:47 +0530

    upload files

---
+++ mixed.c
@@ -0,0 +1,18 @@
+int main()
+{
+    int a = 4 + 5;
+    int z = 0 ;
+    
+        int b = 6 * 9;
+        while(b >  10)
+        {
+           int c = 7;
+           a = 4 + 5;
+            if(c && a){
+                long d = 4;}
+        }
+        if ( z <  5){
+            z = z + 5;
+        }
+
+}
